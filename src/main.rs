b558ede80a73e0fce8a6ce936d00522491db0ae9
Christian Dürr <programming@undeadleech.de>
2016-10-19 19:03:39 +0200

    Merged dotfiles and templates.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // TODO: (Optional) Add theme system
 // TODO: (Optional) Add globals with theme system
-// TODO: Add directory/symlink copy to templates and remove dotfiles.rs
+// TODO: (Optional) Copy empty directories
 extern crate rustc_serialize;
 extern crate handlebars;
 extern crate walkdir;
@@ -10,7 +10,6 @@ extern crate toml;
 mod stylish_templater;
 mod templates;
 mod utilities;
-mod dotfiles;
 mod sqldbs;
 
 use std::env::args;
@@ -19,14 +18,11 @@ fn main() {
     let args: Vec<_> = args().collect();
     if let Some(db_path) = args.get(2) {
         if args.get(1) == Some(&String::from("--templatedb")) {
-            stylish_templater::template_db(&db_path);
+            stylish_templater::template_db(db_path);
         }
     } else {
         if args.len() == 1 || args[1] == "--templates" {
             templates::load();
-        }
-        if args.len() == 1 || args[1] == "--dotfiles" {
-            dotfiles::load();
         }
         if args.len() == 1 || args[1] == "--sqldbs" {
             sqldbs::load();
--- src/templates.rs
+++ src/templates.rs
@@ -1,30 +1,31 @@
+use rustc_serialize::json::Json;
 use handlebars::Handlebars;
-use std::fs::File;
+use walkdir::WalkDir;
+use std::fs::{File, copy, create_dir_all, remove_file, symlink_metadata, read_link};
+use std::os::unix::fs::symlink;
 use std::io::{Read, Write};
+use std::path::Path;
 use utilities;
 
 pub fn load() {
     let variables = match utilities::get_variables_json() {
         Some(c) => c,
-        None => return,
+        None => {
+            println!("Unable to load variables from config.");
+            return;
+        }
     };
 
     let templates = match utilities::load_from_toml("templates") {
         Some(t) => t,
-        None => return,
+        None => {
+            println!("Unable to load templates from config.");
+            return;
+        }
     };
 
     for (template, tar_path) in templates {
-        let mut src_content = String::new();
         let src_path = format!("{}/templates/{}", utilities::get_app_dir(), &template);
-        let _ = match File::open(&src_path) {
-            Ok(mut f) => f.read_to_string(&mut src_content),
-            Err(_) => {
-                println!("Failed copying {}: Unable to read source file", &template);
-                continue;
-            }
-        };
-
         let tar_path = match utilities::path_value_to_string(&tar_path) {
             Some(s) => s,
             None => {
@@ -35,29 +36,77 @@ pub fn load() {
             }
         };
 
-        let handlebars = Handlebars::new();
-        let rendered = match handlebars.template_render(&src_content, &variables) {
-            Ok(rendered_str) => rendered_str,
+        copy_files(&src_path, &tar_path, &variables);
+    }
+}
+
+fn copy_files(src_path: &str, tar_path: &str, variables: &Json) {
+    for file in WalkDir::new(src_path) {
+        let file_path = file.unwrap().path().to_string_lossy().into_owned();
+        let file_tar_path = format!("{}{}", tar_path, &file_path[src_path.len()..]);
+
+        let file_meta = match symlink_metadata(&file_path) {
+            Ok(meta) => meta,
             Err(_) => {
-                println!("Failed copying {}: Unable to render template.", &template);
+                println!("Failed copying {}: Unable to obtain metadata.", &file_path);
                 continue;
             }
         };
 
-        let create_dirs_success = utilities::create_directories_for_file(&tar_path);
-        if !create_dirs_success {
-            println!("Failed copying {}: Could not create one or more directories required.",
-                     &template);
-            continue;
+        if file_meta.is_file() || file_meta.file_type().is_symlink() {
+            if let Err(e) = create_directories_for_file(&file_tar_path) {
+                println!("Failed copying {}: {}", &file_path, &e);
+                continue;
+            }
         }
 
-        let _ = match File::create(&tar_path) {
-            Ok(mut f) => f.write_all(rendered.as_bytes()),
-            Err(_) => {
-                println!("Failed copying {}: Could not create target file.",
-                         &template);
+        if file_meta.is_file() {
+            if let Err(e) = template_file(&file_path, &file_tar_path, variables) {
+                println!("Failed copying {}: {}", &file_path, &e);
                 continue;
             }
-        };
+        } else if file_meta.file_type().is_symlink() {
+            let _ = remove_file(&file_tar_path); // Can't overwrite symlink
+            let sym_tar_path = match read_link(&file_path) {
+                Ok(path) => path,
+                Err(e) => {
+                    println!("Failed copying {}: {}", &file_path, &e);
+                    continue;
+                }
+            };
+            if let Err(e) = symlink(&sym_tar_path, &file_tar_path) {
+                println!("Failed copying {}: {}", &file_path, &e);
+                continue;
+            }
+        }
     }
+}
+
+fn template_file(src_path: &str, tar_path: &str, variables: &Json) -> Result<(), String> {
+    let mut file_content = String::new();
+    let mut f = try!(File::open(src_path).map_err(|e| e.to_string()));
+    match f.read_to_string(&mut file_content) {
+        Ok(s) => s,
+        Err(_) => {
+            // Try copying normall when file is not a text file
+            try!(copy(src_path, tar_path).map_err(|e| e.to_string()));
+            return Ok(());
+        }
+    };
+
+    let handlebars = Handlebars::new();
+    let rendered = try!(handlebars.template_render(&file_content, variables)
+        .map_err(|e| e.to_string()));
+
+    let mut f = try!(File::create(&tar_path).map_err(|e| e.to_string()));
+    try!(f.write_all(rendered.as_bytes()).map_err(|e| e.to_string()));
+    Ok(())
+}
+
+fn create_directories_for_file(file_path: &str) -> Result<(), String> {
+    let _ = match Path::new(&file_path).parent() {
+        Some(p) => create_dir_all(&p),
+        None => return Err(String::from("Could not find the file's parent directory.")),
+    };
+    Ok(())
 }
